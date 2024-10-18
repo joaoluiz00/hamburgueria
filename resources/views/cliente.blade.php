1fc39faf15c4e09ac0ca21d3c965e4ff57036e09
joaoluiz00 <joao.luiz.souza.pereira@gmail.com>
2024-10-18 14:29:33 -0300

    segundo commit

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -1,1 +1,32 @@
 <h1>Pagina do cliente</h1>
+@if (10 > 4)
+    <p> Resposta Verdadeira</p>
+@endif
+
+<p> {{$numero}}</p>
+@if ($numero > 0)
+    <p> Numero Positivo</p>
+@elseif ($numero == 0)
+    <p> Numero nulo</p>
+@else 
+    <p> Numero negativo</p>
+@endif
+
+<p> Exemplo do For</p>
+@for ($i = 0; $i < count($lanches); $i++)
+    <p> {{$lanches[$i]}} - {{$i}} </p>
+@endfor
+
+<p> Exemplo do Foreach</p>
+@foreach ($lanches as $lanches)
+    <p> {{$loop->index}}</p>
+    <p> {{$lanches}}</p>
+@endforeach
+
+<p> Exemplo do PHP </p>
+@php
+    $nome = "Batata frita";
+    echo $nome;
+@endphp
+
+<a href="/"> Voltar para a Home</a>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,20 @@ Route::get('/', function () {
 Route::get('/cliente', function () {
     return view('cliente');
 });
+
+// Route::get('/cliente', function () {
+//     $numero = -1;
+//     return view('cliente',['numero' => $numero]);
+// });
+
+Route::get('/cliente', function () {
+    $numero = -1;
+    $nome = "Admin";
+    $codigo = 12;
+    $lanches = ["X-Salada","X-Bacon","X-Tudo"];
+    return view('cliente',[
+    'numero' => $numero,
+    'nome' => $nome,
+    'codigo' => $codigo,
+    'lanches' => $lanches]);
+    });
